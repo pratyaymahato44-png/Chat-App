ec791d35d388cfd156226dae2ea420deb39eb0ad
Pratyay Mahato <pratyaymahato44@gmail.com>
2026-09-07 18:53:16 +0530

    add server listne

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -9,8 +9,7 @@ const connectDB = async () => {
         
     } catch (error) {
         console.log("MongoDB connection FAILED !!");
-        process.exit(1)
-        
+        process.exit(1)       
     }
 }
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,19 @@
 import "dotenv/config"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
-
+const PORT = process.env.PORT || 8000
 
 
 connectDB()
+.then( () => {
+    app.listen(PORT, () => {
+        console.log("server is running on PORT", PORT)
+    })
+})
+.catch((error) => {
+    console.error("MongoDB connection failed !!!", error)
+})
 
 
 
