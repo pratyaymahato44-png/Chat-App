a0b9f4e2b8e64a88121c36cb30448736ce292c8b
Pratyay Mahato <pratyaymahato44@gmail.com>
2026-09-07 23:04:23 +0530

    integrate clerk authentication by adding clerkProvider and update app component

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,14 @@
 import express from "express"
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
 
 const app = express()
 
-
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+app.use(express.json())
+app.use(clerkMiddleware())
 
 export default app
