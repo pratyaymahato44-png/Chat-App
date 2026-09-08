1cb5bdb390c00c86c9cd4c413e6e49fdc07013e9
Pratyay Mahato <pratyaymahato44@gmail.com>
2026-09-08 09:24:54 +0530

    Add a cron job to send health check request in every 14 minutes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,14 @@
 import "dotenv/config"
 import connectDB from "./db/index.js"
 import app from "./app.js"
+import job from "./lib/cronJob.js"
 
 const PORT = process.env.PORT || 8000
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ok: true})
+})
 
 
 connectDB()
@@ -10,6 +16,10 @@ connectDB()
     app.listen(PORT, () => {
         console.log("server is running on PORT", PORT)
     })
+
+    if(process.env.NODE_ENV === "production"){
+        job.start()
+    }
 })
 .catch((error) => {
     console.error("MongoDB connection failed !!!", error)
