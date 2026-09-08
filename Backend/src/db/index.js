17d5631d255d51d3713eaef051cd2ba9e7ee7ecb
Pratyay Mahato <pratyaymahato44@gmail.com>
2026-09-08 09:04:37 +0530

    Add some debugging

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
         
     } catch (error) {
         console.log("MongoDB connection FAILED !!");
+        console.error(error.message);
         process.exit(1)       
     }
 }
